97d17c3eb451fe23031e11dcecb3f1935b5b8958
Subham Mondal <subham.mondal.23@aot.edu.in>
2025-10-03 12:30:42 +0530

    Add order management functionality: implement order model and controller, create order routes, and integrate with admin routes for order status updates.

---
+++ backend/src/controllers/order-controller.js
@@ -0,0 +1,140 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { Order } from "../models/order-model.js";
+import { Product } from "../models/product-model.js";
+import apiError from "../utils/apiError.js";
+import apiResponse from "../utils/apiResponse.js";
+
+const createOrder = asyncHandler(async(req, res)=>{
+    const { items,shippingAddress, payment } = req.body;
+
+    if([items,shippingAddress,payment].some((field)=> field?.trim === "")){
+        throw new apiError(400, "All fields are required!")
+    }
+
+    for(const item of items){
+        const product = await Product.findById(item.product);
+        if(!product){
+            throw new apiError(404, `Product ${item.name} not found!`);
+        }
+        if(product.stock < item.quantity){
+            throw new apiError(400, `Only ${product.stock} items left in stock for product ${product.name}`);
+        }
+        product.stock -= item.quantity;
+        await product.save();
+        item.price = product.price;
+        item.name = product.name;
+    }
+    const order = await Order.create({
+        user: req.user._id, 
+        items, 
+        totalPrice: items.reduce((total, item) => total + item.price * item.quantity, 0),
+        shippingAddress, 
+        payment:{
+            method: payment,
+            status: payment === "COD" ? "COD" : "pending"
+        }
+    });
+    return res
+    .status(201)    
+    .json(
+        new apiResponse(
+            201,
+            order,
+            "Order created successfully!!!"
+        )
+    )
+})
+
+const getOrders = asyncHandler(async(req, res)=>{
+    const orders = await Order.find({user: req.user._id}).populate("items.product");
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,    
+            orders,
+            "Orders fetched successfully!!!"
+        )
+    )
+})
+
+const getAllOrders = asyncHandler(async(req, res)=>{
+    const orders = await Order.find().populate("items.product").populate("user", "name email");
+    return res
+    .status(200)
+    .json(
+        new apiResponse(
+            200,
+            orders,
+            "All Orders fetched successfully!!!"
+        )
+    )
+})
+
+const updateOrderStatus = asyncHandler(async(req, res)=>{
+    const { orderId } = req.params;
+    const { orderStatus } = req.body;
+    if(!["pending", "confirmed", "shipped", "delivered", "cancelled", "returned"].includes(orderStatus)){
+        throw new apiError(400, "Invalid status value!");
+    }
+    const order = await Order.findById(orderId);
+    if(!order){
+        throw new apiError(404, "Order not found!");
+    }
+    order.orderStatus = orderStatus;
+    order.updatedAt = Date.now();
+    await order.save(); 
+    return res
+    .status(200)
+    .json(  
+        new apiResponse(
+            200,
+            order,
+            "Order status updated successfully!!!"
+        )
+    )
+})
+
+const cancelOrder = asyncHandler(async (req, res)=>{
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+    if(!order){
+        throw new apiError(404, "Order not found!");
+    }
+    if(order.orderStatus === "cancelled"){
+        throw new apiError(400, "Order is already cancelled!");
+    }
+    if(order.orderStatus === "pending" || order.orderStatus === "confirmed"){
+        order.orderStatus = "cancelled";
+        order.updatedAt = Date.now();
+        await order.save();
+
+        for(const item of order.items){
+            const product = await Product.findById(item.product);
+            if(product){
+                product.stock += item.quantity;
+                await product.save();
+            }
+        }
+        
+        return res
+        .status(200)
+        .json(
+            new apiResponse(
+                200,
+                order,
+                "Order cancelled successfully!!!"
+            )
+        )
+    }else{
+        throw new apiError(400, `Order cannot be cancelled as it is already ${order.orderStatus}`);
+    }
+})
+
+export{
+    createOrder,
+    getOrders,
+    getAllOrders,
+    updateOrderStatus,
+    cancelOrder
+}
---
+++ backend/src/models/order-model.js
@@ -0,0 +1,67 @@
+import mongoose, { Schema } from "mongoose";
+
+const orderItemSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    items: [
+        {
+            product: {
+                type: Schema.Types.ObjectId,
+                ref: 'Product',
+                required: true
+            },
+            name:{
+                type: String,
+                required: true,
+                trim: true,
+            },
+            price: {
+                type: Number,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            }
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        required: true
+    },
+    shippingAddress: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    orderStatus: {
+        type: String,
+        enum: ["pending", "confirmed", "shipped", "delivered", "cancelled", "returned"],
+        default: "pending"
+    },
+    payment: {
+        method: { 
+            type: String, 
+            enum: ["COD", "Razorpay", "Stripe"], 
+            required: true 
+        },
+        transactionId: { type: String },
+        status: { 
+            type: String, 
+            enum: ["pending", "paid", "failed", "refunded","COD"], 
+            default: "pending" 
+        }
+    },
+    placedAt:{
+        type:Date,
+        default:Date.now
+    },
+    updatedAt:{
+        type:Date,
+        default:Date.now
+    }
+},{timestamps:true});   
+
+export const Order = mongoose.model("Order", orderItemSchema);
