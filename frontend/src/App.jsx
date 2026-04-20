5ea8000bc389c3b9280d0a43d092aada622194f7
Tomoghna <tomoghanadey622@gmail.com>
2026-04-20 06:07:47 +0000

    connected endpoints for dynamic banner

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useContext, Suspense, lazy } from 'react'
+import React, { useContext, Suspense, lazy, useMemo } from 'react'
 import { Link as RouterLink } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { ProductProvider } from "./contexts/ProductContext";
 import { WishlistProvider } from "./contexts/WishlistContext";
 import { CartProvider } from "./contexts/CartContext";
+import { BannerProvider, useBanners } from "./contexts/BannerContext";
 import { useCategories } from "./contexts/CategoryContext";
 import { Box, Container, Typography, Card, CardMedia, CardContent, Link as MuiLink, useTheme, useMediaQuery } from "@mui/material";
 import Banner from "./components/Banner";
@@ -27,15 +28,17 @@ const LoginPage = lazy(() => import("./pages/LoginPage"));
 const Orders = lazy(() => import("./pages/OrdersPage"));
 const OrderSuccessPage = lazy(() => import("./pages/OrderSuccessPage"));
 
-export default function App() {
+function AppContent() {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
   const isTablet = useMediaQuery(theme.breakpoints.down('md'));
   const { categories, loading: loadingCategories, error, refreshCategories } = useCategories();
+  const { banners } = useBanners();
 
-  const images = [
-    "/products/product-1.jpg",
-  ];
+  // Extract all images from banners
+  const images = useMemo(() => {
+    return banners.flatMap(banner => (Array.isArray(banner.image) ? banner.image : [banner.image])).filter(img => img);
+  }, [banners]);
 
   const PageSkeleton = () => {
   return (
@@ -100,10 +103,7 @@ export default function App() {
 
 
   return (
-    <ProductProvider>
-      <WishlistProvider>
-        <CartProvider>
-          <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
+    <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
             <Router>
               <Navbar />
               {/* Global horizontal margin wrapper */}
@@ -226,6 +226,17 @@ export default function App() {
               <BackToTop />
             </Router>
           </Box>
+  );
+}
+
+export default function App() {
+  return (
+    <ProductProvider>
+      <WishlistProvider>
+        <CartProvider>
+          <BannerProvider>
+            <AppContent />
+          </BannerProvider>
         </CartProvider>
       </WishlistProvider>
     </ProductProvider>
